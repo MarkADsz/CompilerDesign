bfe3d3fba2bcd2726914fc61f02dcf800cfa4739
Doszlop-Mark <doszlopmark@yahoo.com>
2023-12-15 17:23:50 +0200

    Lab6

--- Lab3/src/Main.java
+++ Lab3/src/Main.java
@@ -1,60 +1,61 @@
 import grammar.Grammar;
 import parser.Parser;
-import scanner.FiniteAutomata;
-import scanner.MyScanner;
-
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         Grammar g = new Grammar("C:\\UBB_FMI\\FLCD\\GitHub\\CompilerDesign\\Lab3\\input\\g1.txt");
+        Parser p = new Parser(g);
+        parserTests();
         while(true){
             System.out.println("1. Set of non terminals");
             System.out.println("2. Set of terminals");
             System.out.println("3. Set of productions");
             System.out.println("4. Productions for a given non terminal");
             System.out.println("5. CFG check");
-            System.out.println("6. Exit");
+            System.out.println("6. Compute first function for a given symbol");
+            System.out.println("7. Compute follow function for a given symbol");
+            System.out.println("8. Exit");
             var option = new java.util.Scanner(System.in).nextInt();
-            if(option == 6) {
+            if(option == 8) {
                 break;
-            }else if(option > 5 || option < 1){
+            }else if (option == 6){
+                String symbol = new java.util.Scanner(System.in).nextLine();
+                String first = p.first(symbol, new StringBuilder());
+                System.out.println(first);
+            }else if (option == 7){
+                String symbol = new java.util.Scanner(System.in).nextLine();
+                String follow = p.follow(symbol, new StringBuilder());
+                System.out.println(follow);
+            }else if(option > 7 || option < 1){
                 System.out.println("Invalid option");
             } else{
                 g.doAction(option);
             }
         }
-        Parser parser=new Parser(g);
-        parserTests();
-
     }
 
+
     static void parserTests(){
-        Grammar yourGrammar = new Grammar("C:\\UBB_FMI\\FLCD\\GitHub\\CompilerDesign\\Lab3\\input\\g1.txt");
+        Grammar g = new Grammar("inputs/g1.txt");
+        Parser p = new Parser(g);
 
-        Parser parser = new Parser(yourGrammar);
+        assertEqualLists("First(S)", p.first("S", new StringBuilder()), "a");
+        assertEqualLists("First(B)", p.first("B", new StringBuilder()), "c");
+        assertEqualLists("First(C)", p.first("C", new StringBuilder()), "bε");
+        assertEqualLists("First(D)", p.first("D", new StringBuilder()), "gε");
+        assertEqualLists("First(E)", p.first("E", new StringBuilder()), "gε");
+        assertEqualLists("First(F)", p.first("F", new StringBuilder()), "fε");
 
-        // Test First sets
-        assertEqualLists("First(E)", parser.first("E"), Arrays.asList("(", "i"));
-        assertEqualLists("First(R)", parser.first("R"), Arrays.asList("+"));
-        assertEqualLists("First(T)", parser.first("T"), Arrays.asList("(", "i"));
-        assertEqualLists("First(Y)", parser.first("Y"), Arrays.asList("*"));
-        assertEqualLists("First(F)", parser.first("F"), Arrays.asList("(", "i"));
-
-        // Test Follow sets
-//        assertEqualLists("Follow(E)", parser.follow("E", new ArrayList<>()), Arrays.asList("$", ")"));
-//        assertEqualLists("Follow(R)", parser.follow("R", new ArrayList<>()), Arrays.asList("$", ")"));
-//        assertEqualLists("Follow(T)", parser.follow("T", new ArrayList<>()), Arrays.asList("+", "$", ")"));
-//        assertEqualLists("Follow(Y)", parser.follow("Y", new ArrayList<>()), Arrays.asList("+", "$", ")"));
-//        assertEqualLists("Follow(F)", parser.follow("F", new ArrayList<>()), Arrays.asList("*", "+", "$", ")"));
-
+        assertEqualLists("Follow(S)", p.follow("S", new StringBuilder()), "$");
+        assertEqualLists("Follow(B)", p.follow("B", new StringBuilder()), "gh");
+        assertEqualLists("Follow(C)", p.follow("C", new StringBuilder()), "gh");
+        assertEqualLists("Follow(D)", p.follow("D", new StringBuilder()), "h");
+        assertEqualLists("Follow(E)", p.follow("E", new StringBuilder()), "h");
         System.out.println("All tests passed!");
     }
 
-    private static <T> void assertEqualLists(String name, List<T> actual, List<T> expected) {
+    private static <T> void assertEqualLists(String name, String actual, String expected) {
         if (!actual.equals(expected)) {
             System.out.println(name + " - Test FAILED");
             System.out.println("Expected: " + expected);
--- Lab3/src/parser/Parser.java
+++ Lab3/src/parser/Parser.java
@@ -10,53 +10,52 @@ public class Parser {
 
     public Parser(Grammar grammar){
         this.grammar=grammar;
-//        this.firsts=new ArrayList<>();
-
     }
 
-    private List<String> first(String nonTerminal, List<String> firsts){
+    public String first(String nonTerminal, StringBuilder firsts){
         for(String element:this.grammar.getProductions().get(nonTerminal)){
             String firstOfElement=String.valueOf(element.charAt(0));
             if(this.grammar.getTerminals().contains(firstOfElement) || firstOfElement.equals("ε")){
-                firsts.add(firstOfElement);
+                firsts.append(firstOfElement);
             }
             else if(this.grammar.getNonTerminals().contains(firstOfElement)){
-                    first(firstOfElement,firsts);
-                }
+                first(firstOfElement,firsts);
+            }
         }
-        return firsts;
+        return firsts.toString();
     }
 
-    public List<String> first(String nonTerminal){
-        return this.first(nonTerminal,new ArrayList<>());
-    }
-
-    public List<String> follow(String nonTerminal, List<String> result) {
+    public String follow(String nonTerminal, StringBuilder result) {
         if(nonTerminal.equals(this.grammar.getStartingSymbol())){
-            result.add("$");
+            result.append("$");
+            return result.toString();
         }
-        for(Map.Entry<String,List<String>> production: this.grammar.getProductions().entrySet()){
-            for(String productionRHS:production.getValue()){
-                if(productionRHS.contains(nonTerminal)){
-                    //placement of the nonterminal
-                    int index=productionRHS.indexOf(nonTerminal);
-                    //if there is nothing after we add the Follow of LHS
-                    if(index==productionRHS.length()-1){
-                            result.addAll(follow(String.valueOf(productionRHS.charAt(index)),new ArrayList<>()));
-                    }else
-                        //if there is a nonterminal after the index
-                        if(this.grammar.getNonTerminals().contains(String.valueOf(productionRHS.charAt(index+1)))){
-                            result.addAll(this.first(String.valueOf(productionRHS.charAt(index+1)),new ArrayList<>()));
-                        } else if (this.grammar.getTerminals().contains(String.valueOf(productionRHS.charAt(index+1)))) { //if there is a terminal after index
-                            result.add(String.valueOf(productionRHS.charAt(index+1)));
+        for(Map.Entry<String, List<String>> entry : this.grammar.getProductions().entrySet()){
+            for(String production : entry.getValue()){
+                for(int i=0; i<production.length(); i+=1){
+                    if(String.valueOf(production.charAt(i)).equals(nonTerminal)){
+                        if(i == production.length() - 1 && !Objects.equals(entry.getKey(), String.valueOf(production.charAt(i)))){
+                            result.append(follow(entry.getKey(), new StringBuilder()));
                         }
+                        else for(int j=i+1; j<production.length(); j+=1){
+                            if (this.grammar.getNonTerminals().contains(String.valueOf(production.charAt(j)))) {
+                                String first = first(String.valueOf(production.charAt(j)), new StringBuilder());
+                                if(!first.contains("ε")){
+                                    result.append(first);
+                                    break;
+                                } else{
+                                    result.append(first.replace("ε",""));
+                                }
+                            } else if (this.grammar.getTerminals().contains(String.valueOf(production.charAt(j)))) {
+                                result.append(production.charAt(j));
+                                break;
+                            }
+                        }
+                    }
                 }
             }
-
-
         }
-
-    return result;
+        return result.toString();
     }
 
 }
